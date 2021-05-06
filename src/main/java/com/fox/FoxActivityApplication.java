3400dbd3e023194831e051e1350b5f6cf1d5fd63
luoyangwei <luoyangwei@baiying365.com>
2021-05-06 13:17:22 +0800

    Create project

---
+++ src/main/java/com/fox/FoxActivityApplication.java
@@ -0,0 +1,13 @@
+package com.fox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxActivityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxActivityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fox/FoxActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
